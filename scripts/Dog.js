3471e58a7746e8da54ef23c33323fbacb23ffd0e
Brandon Rodriguez <chilebr99@gmail.com>
2022-12-29 14:26:14 -0500

    Added colors and images on button interactions

--- scripts/Dog.js
+++ scripts/Dog.js
@@ -6,6 +6,7 @@ class Dogs {
     getDogInfoHtml() {
       const { name, avatar, bio,} = this;
       return `  <img src="${avatar}" alt="" />
+      <div class="nope-like-img"></div>
       <div class="dog-info">
       <p>${name}</p>
       <p>${bio}</p>
--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,28 @@ import Dogs from "./Dog.js";
 
 const disliked = document.getElementById("dislike");
 const liked = document.getElementById("liked");
+const imgCreate = document.createElement("img")
+
+
+document.querySelector(".buttons").addEventListener('click', (event) => {
+  const isButton = event.target.nodeName === 'BUTTON';
+  if (!isButton) {
+    return;
+  }
+if (event.target.id === "dislike") {
+  document.getElementById("dislike").style.backgroundColor =  "#FFE7EF"
+  imgCreate.src = "images/badge-nope.png"
+  document.querySelector(".nope-like-img").appendChild(imgCreate)
+  
+} else if (event.target.id === "liked") {
+  document.getElementById("liked").style.backgroundColor = "#DBFFF4"
+  imgCreate.src = "images/badge-like.png"
+  document.querySelector(".nope-like-img").appendChild(imgCreate)
+}
+  
+})
+
+
 
 
 
@@ -15,3 +37,4 @@ let dogInfo = new Dogs(dogs.Teddy)
 
 
 render()
+
